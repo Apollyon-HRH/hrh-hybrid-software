727c1e522f974c88b734e93e26aa11f98fee1241
Apollyon-HRH <apollyon.hrh.team@proton.me>
2026-05-26 09:37:36 -0300

    Update vehicleController.lua

--- lua/vehicleController.lua
+++ lua/vehicleController.lua
@@ -1,105 +1,43 @@
--- HRH Hybrid Powertrain - Team Apollyon
--- Versão autônoma: funciona sem vehicleController.lua
+-- vehicleController.lua (na raiz do veículo)
+-- Controller principal do veículo HRH - Team Apollyon
 
 local M = {}
 
--- Parâmetros (Apêndice C5.1)
-local BATTERY_CAPACITY_KWH = 8.0
-local MGUF_MAX_POWER_KW = 325
-local MGUR_MAX_POWER_KW = 325
-local REGEN_MAX_POWER_KW = 250
-local MAX_TORQUE_NM = 500
+-- CORREÇÃO AQUI: Carrega os módulos com 'require' e caminhos absolutos!
+local hrh_hybrid = require('lua/vehicle/extensions/auto/hrh_hybrid')
+local data_logger = require('lua/vehicle/extensions/auto/data_logger')
+local traction_control = require('lua/vehicle/extensions/auto/traction_control')
+local regen_braking = require('lua/vehicle/extensions/auto/regenerative_braking')
+local pit_limiter = require('lua/vehicle/extensions/auto/pit_limiter')
+local abs_offroad = require('lua/vehicle/extensions/auto/abs_offroad')
 
-local soc = 1.0
-local kill_switch_activated = false
-local vehicle = nil
+function M.onInit()
+    print("HRH: Inicializando sistema...")
 
--- Converte kW para Nm (aproximação linear)
-local function power_to_torque(power_kw)
-    return power_kw * (MAX_TORQUE_NM / MGUF_MAX_POWER_KW)
-end
-
--- Aplica potência diretamente nos valores de throttle do motor elétrico
-local function apply_throttle(motor_throttle_name, torque_nm)
-    if not vehicle then return end
-    vehicle.electrics.values[motor_throttle_name] = torque_nm
-end
-
--- Função chamada quando a extensão é carregada
-function M.onExtensionLoaded()
-    print("HRH Hybrid: Sistema inicializado (autônomo).")
-    
-    -- Obtém a referência do veículo através da API de extensões
-    vehicle = extensions.getVehicle()
-    if vehicle then
-        print("HRH Hybrid: Veículo referenciado com sucesso.")
+    -- 1. Inicializa o sistema híbrido (passa a referência do veículo)
+    if hrh_hybrid and hrh_hybrid.setVehicle then
+        hrh_hybrid.setVehicle(vehicle)
     else
-        print("HRH Hybrid: ERRO - Não foi possível obter referência do veículo.")
+        print("HRH: ERRO - Módulo hrh_hybrid não encontrado!")
     end
 
-    -- Cria a tabela de debug global para uso na consola
-    _G.hrh_debug = {
-        get_soc = function() return soc end,
-        kill = function() M.emergency_kill() end,
-        status = function()
-            print("SOC: " .. string.format("%.2f", soc * 100) .. "%")
-            print("Kill switch: " .. tostring(kill_switch_activated))
-            if vehicle then
-                print("mguf throttle: " .. tostring(vehicle.electrics.values.mguf_throttle))
-                print("mgur throttle: " .. tostring(vehicle.electrics.values.mgur_throttle))
-            else
-                print("Vehicle reference is nil")
-            end
-        end,
-        set_throttle = function(value)
-            value = math.min(1, math.max(0, value))
-            local torque = power_to_torque(value * MGUF_MAX_POWER_KW)
-            apply_throttle("mguf_throttle", torque)
-            apply_throttle("mgur_throttle", torque)
-            print("Set throttle to " .. value .. " -> torque " .. torque .. " Nm")
-        end
-    }
-    print("HRH: Debug functions available. Type 'hrh_debug.status()' in console.")
+    -- 2. Inicializa os outros sistemas
+    if data_logger and data_logger.start_logging then data_logger.start_logging(vehicle) end
+    if traction_control and traction_control.init then traction_control.init(vehicle, hrh_hybrid) end
+    if regen_braking and regen_braking.init then regen_braking.init(vehicle, hrh_hybrid) end
+    if pit_limiter and pit_limiter.init then pit_limiter.init(vehicle) end
+    if abs_offroad and abs_offroad.init then abs_offroad.init(vehicle) end
+
+    print("HRH: Todos os sistemas inicializados. Digite 'hrh_debug.status()' no console.")
 end
 
--- Kill switch
-function M.emergency_kill()
-    if kill_switch_activated then return end
-    kill_switch_activated = true
-    apply_throttle("mguf_throttle", 0)
-    apply_throttle("mgur_throttle", 0)
-    print("HRH Hybrid: KILL SWITCH ACTIVATED!")
-end
-
--- Atualização periódica (chamada automaticamente pela extensão)
 function M.update(dt)
-    if not vehicle or kill_switch_activated then return end
-
-    local throttle_input = vehicle.electrics.values.throttle_input or 0
-    local brake_input = vehicle.electrics.values.brake_input or 0
-
-    -- Potência desejada
-    local desired_power_front = throttle_input * MGUF_MAX_POWER_KW
-    local desired_power_rear = throttle_input * MGUR_MAX_POWER_KW
-    local front_torque = power_to_torque(desired_power_front)
-    local rear_torque = power_to_torque(desired_power_rear)
-
-    apply_throttle("mguf_throttle", front_torque)
-    apply_throttle("mgur_throttle", rear_torque)
-
-    -- Simulação simples da bateria
-    local total_power_kw = (desired_power_front + desired_power_rear) * throttle_input
-    local energy_used_kwh = total_power_kw * (dt / 3600)
-    soc = soc - (energy_used_kwh / BATTERY_CAPACITY_KWH)
-    soc = math.max(0, math.min(1, soc))
-
-    -- Travagem regenerativa
-    if brake_input > 0 then
-        local regen_power = brake_input * REGEN_MAX_POWER_KW
-        local regen_torque = power_to_torque(regen_power)
-        apply_throttle("mguf_throttle", -regen_torque)
-        apply_throttle("mgur_throttle", -regen_torque)
-    end
+    -- Atualiza todos os sistemas a cada frame
+    if hrh_hybrid and hrh_hybrid.update then hrh_hybrid.update(dt) end
+    if traction_control and traction_control.update then traction_control.update(dt) end
+    if regen_braking and regen_braking.update then regen_braking.update(dt) end
+    if pit_limiter and pit_limiter.update then pit_limiter.update(dt) end
+    if abs_offroad and abs_offroad.update then abs_offroad.update(dt) end
 end
 
 return M
