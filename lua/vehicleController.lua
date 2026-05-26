2623042a54bfe4ed7d255124cc48e4546aa752d9
Apollyon-HRH <apollyon.hrh.team@proton.me>
2026-05-26 03:29:32 -0300

    Update vehicleController.lua

--- lua/vehicleController.lua
+++ lua/vehicleController.lua
@@ -1,41 +1,43 @@
--- vehicleController.lua (na raiz do veículo)
+-- vehicleController.lua (Na RAÍZ do veículo)
 -- Controller principal do veículo HRH - Team Apollyon
-
 local M = {}
 
--- Caminhos absolutos a partir da raiz do veículo
-local hrh_hybrid = require('lua/vehicle/extensions/auto/hrh_hybrid')
-local data_logger = require('lua/vehicle/extensions/auto/data_logger')
-local traction_control = require('lua/vehicle/extensions/auto/traction_control')
-local regen_braking = require('lua/vehicle/extensions/auto/regenerative_braking')
-local pit_limiter = require('lua/vehicle/extensions/auto/pit_limiter')
-local abs_offroad = require('lua/vehicle/extensions/auto/abs_offroad')
+-- Carrega as extensões da pasta 'extensions/auto'
+-- O 'extensions.load' carrega o módulo e o retorna.
+local hrh_hybrid = extensions.load('hrh_hybrid')
+local data_logger = extensions.load('data_logger')
+local traction_control = extensions.load('traction_control')
+local regen_braking = extensions.load('regenerative_braking')
+local pit_limiter = extensions.load('pit_limiter')
+local abs_offroad = extensions.load('abs_offroad')
 
 function M.onInit()
     print("HRH: Inicializando sistema...")
 
-    if hrh_hybrid then
+    -- 1. Inicializa o sistema híbrido (passa a referência do veículo)
+    if hrh_hybrid and hrh_hybrid.setVehicle then
         hrh_hybrid.setVehicle(vehicle)
-        print("HRH: Hybrid module initialized.")
     else
         print("HRH: ERRO - Módulo hrh_hybrid não encontrado!")
     end
 
-    if data_logger then data_logger.start_logging(vehicle) end
-    if traction_control then traction_control.init(vehicle, hrh_hybrid) end
-    if regen_braking then regen_braking.init(vehicle, hrh_hybrid) end
-    if pit_limiter then pit_limiter.init(vehicle) end
-    if abs_offroad then abs_offroad.init(vehicle) end
+    -- 2. Inicializa os outros sistemas
+    if data_logger and data_logger.start_logging then data_logger.start_logging(vehicle) end
+    if traction_control and traction_control.init then traction_control.init(vehicle, hrh_hybrid) end
+    if regen_braking and regen_braking.init then regen_braking.init(vehicle, hrh_hybrid) end
+    if pit_limiter and pit_limiter.init then pit_limiter.init(vehicle) end
+    if abs_offroad and abs_offroad.init then abs_offroad.init(vehicle) end
 
-    print("HRH: Todos os sistemas inicializados.")
+    print("HRH: Todos os sistemas inicializados. Digite 'hrh_debug.status()' no console.")
 end
 
 function M.update(dt)
-    if hrh_hybrid then hrh_hybrid.update(dt) end
-    if traction_control then traction_control.update(dt) end
-    if regen_braking then regen_braking.update(dt) end
-    if pit_limiter then pit_limiter.update(dt) end
-    if abs_offroad then abs_offroad.update(dt) end
+    -- Atualiza todos os sistemas a cada frame
+    if hrh_hybrid and hrh_hybrid.update then hrh_hybrid.update(dt) end
+    if traction_control and traction_control.update then traction_control.update(dt) end
+    if regen_braking and regen_braking.update then regen_braking.update(dt) end
+    if pit_limiter and pit_limiter.update then pit_limiter.update(dt) end
+    if abs_offroad and abs_offroad.update then abs_offroad.update(dt) end
 end
 
 return M
