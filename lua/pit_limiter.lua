8c37241f088863dc6b7dafc9a1dcfbd309ef890a
Apollyon-HRH <apollyon.hrh.team@proton.me>
2026-05-26 02:29:44 -0300

    Update pit_limiter.lua

--- lua/pit_limiter.lua
+++ lua/pit_limiter.lua
@@ -1,10 +1,7 @@
--- HRH Pit Limiter - Team Apollyon
--- Conforme Art. C7.7, limitando a velocidade a 60 km/h.
-
 local M = {}
 local vehicle = nil
-local pit_limiter_active = false
-local PIT_LIMIT_SPEED_MS = 16.67  -- 60 km/h em m/s
+local active = false
+local PIT_LIMIT_SPEED_KPH = 60
 
 function M.init(vehicle_obj)
     vehicle = vehicle_obj
@@ -12,26 +9,18 @@ function M.init(vehicle_obj)
 end
 
 function M.update(dt)
-    if not vehicle or not pit_limiter_active then return end
-
-    local current_speed_ms = vehicle:getSpeed()
-    if current_speed_ms > PIT_LIMIT_SPEED_MS then
-        -- Força o throttle a 0 se a velocidade exceder o limite
-        -- Nota: Isto pode ser melhorado com um controlo PID para maior suavidade
+    if not vehicle or not active then return end
+    local speed_kph = vehicle:getSpeed() * 3.6
+    if speed_kph > PIT_LIMIT_SPEED_KPH then
         vehicle.electrics.values.throttle_input = 0
-        vehicle.electrics.values.throttle = 0
+        vehicle.electrics.values.brake_input = 1
     end
 end
 
 function M.toggle()
-    pit_limiter_active = not pit_limiter_active
-    print("HRH Pit Limiter: " .. (pit_limiter_active and "Ativado" or "Desativado"))
-    return pit_limiter_active
-end
-
-function M.set_active(active)
-    pit_limiter_active = active
-    print("HRH Pit Limiter: " .. (pit_limiter_active and "Ativado" or "Desativado"))
+    active = not active
+    print("HRH Pit Limiter: " .. (active and "Ativado" or "Desativado"))
+    return active
 end
 
 return M
