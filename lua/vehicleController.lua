12c700fb30ca743d92e65045c8e1a0b529888953
Apollyon-HRH <apollyon.hrh.team@proton.me>
2026-05-26 02:28:43 -0300

    Update vehicleController.lua

--- lua/vehicleController.lua
+++ lua/vehicleController.lua
@@ -3,22 +3,19 @@
 
 local M = {}
 
--- Carrega os módulos da pasta 'extensions/auto'
--- O caminho correto é relativo à raiz do veículo
-local hrh_hybrid = require('lua/vehicle/extensions/auto/hrh_hybrid')
-local data_logger = require('lua/vehicle/extensions/auto/data_logger')
-local traction_control = require('lua/vehicle/extensions/auto/traction_control')
-local regen_braking = require('lua/vehicle/extensions/auto/regenerative_braking')
-local pit_limiter = require('lua/vehicle/extensions/auto/pit_limiter')
-local abs_offroad = require('lua/vehicle/extensions/auto/abs_offroad')
+-- Caminho relativo à raiz da pasta 'lua/vehicle/'
+local hrh_hybrid = require('vehicle/extensions/auto/hrh_hybrid')
+local data_logger = require('vehicle/extensions/auto/data_logger')
+local traction_control = require('vehicle/extensions/auto/traction_control')
+local regen_braking = require('vehicle/extensions/auto/regenerative_braking')
+local pit_limiter = require('vehicle/extensions/auto/pit_limiter')
+local abs_offroad = require('vehicle/extensions/auto/abs_offroad')
 
 function M.onInit()
     print("HRH: Inicializando sistema...")
 
-    -- Registra o veículo no sistema híbrido
     hrh_hybrid.setVehicle(vehicle)
-    
-    -- Inicializa os outros sistemas
+
     data_logger.start_logging(vehicle)
     traction_control.init(vehicle, hrh_hybrid)
     regen_braking.init(vehicle, hrh_hybrid)
@@ -29,7 +26,6 @@ function M.onInit()
 end
 
 function M.update(dt)
-    -- Atualiza todos os sistemas em cada frame
     hrh_hybrid.update(dt)
     traction_control.update(dt)
     regen_braking.update(dt)
